f8399cb3c988ad506960c7d951cfe1cdb66df54b
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-12 16:13:46 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,13 @@
 class Artist
 
   attr_reader :name, :years_experience
+  
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
 end
